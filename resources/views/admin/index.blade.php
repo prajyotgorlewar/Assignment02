32f2343686e41fb821dc34354a7b17a9841ebbdc
prajyot gorlewar <prajyotgorlewargmail.com>
2023-12-19 08:20:35 +0530

    Added AdminController with admin.index view

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,7 @@
+<x-admin-layout>
+    <x-slot name="header">
+        {{ __('New Admin Section')}}
+    </x-slot>
+
+    This is the content of my admin home
+</x-admin-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +30,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin', [
+    AdminController::class, 'index'
+])->middleware('auth')->name('admin');
