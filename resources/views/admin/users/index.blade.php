7d0916f835e7bb86cae65db08493cdec3184c76c
prajyot gorlewar <prajyotgorlewargmail.com>
2023-12-23 23:04:04 +0530

    Added admin middleware

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -5,7 +5,7 @@
     <div class="flex flex-col">
 
         @if(Session::has('admin_flash'))
-            <x-alert type="error" position="top-right">{{ Session('admin_flash') }}</x-alert>
+            <x-alert type="success" position="top-right">{{ Session('admin_flash') }}</x-alert>
         @endif
         <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
             <div class="overflow-hidden">
