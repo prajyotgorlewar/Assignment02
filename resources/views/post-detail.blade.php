8e52a2af3e2a54d25412b3b0a8af205fe75163a6
prajyot gorlewar <prajyotgorlewargmail.com>
2023-12-31 19:28:34 +0530

    Done some changes in post detail

--- resources/views/post-detail.blade.php
+++ resources/views/post-detail.blade.php
@@ -1,12 +1,12 @@
 <x-guest-layout>
     <x-slot name="header">
-        {{ __('Our Articles') }}
+        {{ __('Simple Post') }}
     </x-slot>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         @if($post)
         <section class="prose p-6">
             <header>
-                <h2>{{ $post->title }}</h2>
+                <h1><b>{{ $post->title }}</b></h1>
             </header>
             <main>{!! $post->content !!}</main>
         </section>
